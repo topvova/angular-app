d531232ea421abb5e3bd7ea0d945d6889ba25497
topvova <topvova@gmail.com>
2017-06-25 23:13:39 +0300

    Add Bootstrap for styling

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import {FormsModule} from "@angular/forms";
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    // to run two-way binding with forms
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
