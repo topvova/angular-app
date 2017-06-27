32a1809277a3cb8eb2261e6e22ebd3e56276ce80
topvova <topvova@gmail.com>
2017-06-27 14:06:02 +0300

    Add styles to app component using url and style declaration (l18)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,8 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  // styleUrls: ['./app.component.css']
+  styles: [`h3 {color: lightseagreen;}`]
 })
 export class AppComponent {
 }
--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -3,7 +3,6 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-servers',
   template: `<app-server></app-server>
-             <app-server></app-server>
              <app-server></app-server>`,
   styleUrls: ['./servers.component.css']
 })
