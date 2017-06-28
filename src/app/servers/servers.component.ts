3fb11f78efb8f99bfcb512168fc0d00cd656e614
topvova <topvova@gmail.com>
2017-06-28 22:16:40 +0300

    Add method onUpdateServerName and pass it  object (l25)

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -11,6 +11,7 @@ import { Component, OnInit } from '@angular/core';
 export class ServersComponent implements OnInit {
   allowNewServer = false;
   serverCreationStatus = 'No server has created yet...';
+  serverName = '';
 
   constructor() {
       setTimeout(() => {
@@ -23,6 +24,11 @@ export class ServersComponent implements OnInit {
 
   onCreateServer() {
       this.serverCreationStatus = 'Server has just created!';
+  }
+
+  onUpdateServerName(event: Event) {
+      // console.log(event);
+      this.serverName = (<HTMLInputElement>event.target).value;
   }
 
 }
