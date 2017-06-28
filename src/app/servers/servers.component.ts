b6edecdd44bfa8cb580261da7aa2c94ebec45896
topvova <topvova@gmail.com>
2017-06-28 20:57:54 +0300

    Add button to the server component using property binding, fill the constructor (l22)

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -1,14 +1,21 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
+  /*selector: '[app-servers]',
+  selector: '.app-servers',*/
   selector: 'app-servers',
-  template: `<app-server></app-server>
-             <app-server></app-server>`,
+  templateUrl: './servers.component.html',
+  /*template: `<app-server></app-server><app-server></app-server>`,*/
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent implements OnInit {
+  allowNewServer = false;
 
-  constructor() { }
+  constructor() {
+      setTimeout(() => {
+          this.allowNewServer = true;
+      }, 2000)
+  }
 
   ngOnInit() {
   }
