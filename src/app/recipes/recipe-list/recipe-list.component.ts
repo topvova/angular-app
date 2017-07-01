2d966f16fda848c159e648d217a47518d4cedceb
topvova <topvova@gmail.com>
2017-07-01 23:08:26 +0300

    Output a list of recipes using *ngFor (l43)

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -14,7 +14,12 @@ export class RecipeListComponent implements OnInit {
           ' carrots—and does just the right thing to them, which is very little. With a simple vinaigrette, some' +
           ' thinly sliced shallots, and a generous serving of fresh ricotta cheese to add richness, it`s all they' +
           ' need to taste their best.', 'http://www.seriouseats.com/recipes/assets_c/2016/05/20160503-fava-carrot-' +
-          'ricotta-salad-recipe-1-thumb-1500xauto-431710.jpg')
+          'ricotta-salad-recipe-1-thumb-1500xauto-431710.jpg'),
+      new Recipe('30-Minute Pressure Cooker Chicken With Chickpeas, Tomatoes, and Chorizo', 'The secret to this recipe' +
+          ' is the addition of canned fire-roasted tomatoes, which add lots of flavor with hardly any effort. ' +
+          'We bolster the dish`s savory side with Spanish chorizo and smoked paprika and add in chicken and canned' +
+          ' chickpeas for a dinner that`s as hearty as it is easy.', 'http://www.seriouseats.com/images/2015/09/' +
+          '20150914-pressure-cooker-recipes-roundup-09.jpg')
   ];
 
   constructor() { }
