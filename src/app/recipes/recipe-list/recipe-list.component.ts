423a90b6987b7bb57b35b57b70fea7fb35940542
topvova <topvova@gmail.com>
2017-07-01 22:43:40 +0300

    Create recipes array, add content for the Recipes components (l42)

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,11 +1,21 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.css']
 })
+
 export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+      new Recipe('Easy Fava Bean and Carrot Salad With Ricotta Recipe',
+          'This quick and easy salad takes some of spring`s best produce—bright green fava beans and tender young' +
+          ' carrots—and does just the right thing to them, which is very little. With a simple vinaigrette, some' +
+          ' thinly sliced shallots, and a generous serving of fresh ricotta cheese to add richness, it`s all they' +
+          ' need to taste their best.', 'http://www.seriouseats.com/recipes/assets_c/2016/05/20160503-fava-carrot-' +
+          'ricotta-salad-recipe-1-thumb-1500xauto-431710.jpg')
+  ];
 
   constructor() { }
 
