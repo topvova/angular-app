49bd1778b60e5331a744588b6548856f98f5e651
topvova <topvova@gmail.com>
2017-07-03 11:17:59 +0300

    Add View Encapsulation property to App Component (l62)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
-import { Component } from '@angular/core';
+import {Component, ViewEncapsulation} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   // styleUrls: ['./app.component.css']
-  styles: [`h3 {color: lightseagreen;}`]
+  styles: [`h3 {color: lightseagreen;}`],
+  encapsulation: ViewEncapsulation.Emulated // Native, None
+
 })
 export class AppComponent {
 }
