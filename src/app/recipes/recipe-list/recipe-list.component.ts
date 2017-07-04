2501af92ee686704fbe9b34667c3a2767bf24a41
topvova <topvova@gmail.com>
2017-07-04 14:50:37 +0300

    Pass recipe between components witn property binding and event emitting (l74|7:58)

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, EventEmitter, OnInit, Output} from '@angular/core';
 import { Recipe } from '../recipe.model';
 
 @Component({
@@ -21,10 +21,15 @@ export class RecipeListComponent implements OnInit {
           ' chickpeas for a dinner that`s as hearty as it is easy.', 'http://www.seriouseats.com/images/2015/09/' +
           '20150914-pressure-cooker-recipes-roundup-09.jpg')
   ];
+  @Output() recipeWasSelected = new EventEmitter<Recipe>();
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onRecipeSelected(recipe: Recipe) {
+      this.recipeWasSelected.emit(recipe);
   }
 
 }
