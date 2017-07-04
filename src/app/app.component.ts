4f0ce7463acae05afd3ee41e8456ff6eb881449a
topvova <topvova@gmail.com>
2017-07-04 14:03:34 +0300

    Add navigation with ngIf and event binding (l72)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  loadedChapter = 'recipe';
 
+  onNavigate(chapter: string) {
+    this.loadedChapter = chapter;
+  }
 }
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component, EventEmitter, Output} from '@angular/core';
 
 @Component({
     selector: 'app-header',
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
 })
 
 export class HeaderComponent {
+    @Output() chapterSelected = new EventEmitter<string>();
 
+    onSelect(chapter: string){
+        this.chapterSelected.emit(chapter);
+    }
 }
