41272fca47c965cc38211b333b5e3e081cd62f3a
topvova <topvova@gmail.com>
2017-07-09 20:01:22 +0300

    Add ingredients to RecipeDetail component (l103)

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,5 +1,6 @@
 import {Recipe} from './recipe.model';
 import {EventEmitter} from '@angular/core';
+import {Ingredient} from '../shared/ingredient.model';
 
 export class RecipeService {
     recipeSelected = new EventEmitter<Recipe>();
@@ -8,13 +9,25 @@ export class RecipeService {
           'This quick and easy salad takes some of spring`s best produce—bright green fava beans and tender young' +
           ' carrots—and does just the right thing to them, which is very little. With a simple vinaigrette, some' +
           ' thinly sliced shallots, and a generous serving of fresh ricotta cheese to add richness, it`s all they' +
-          ' need to taste their best.', 'http://www.seriouseats.com/recipes/assets_c/2016/05/20160503-fava-carrot-' +
-          'ricotta-salad-recipe-1-thumb-1500xauto-431710.jpg'),
-      new Recipe('30-Minute Pressure Cooker Chicken With Chickpeas, Tomatoes, and Chorizo', 'The secret to this recipe' +
-          ' is the addition of canned fire-roasted tomatoes, which add lots of flavor with hardly any effort. ' +
+          ' need to taste their best.',
+          'http://www.seriouseats.com/recipes/assets_c/2016/05/20160503-fava-carrot-' +
+          'ricotta-salad-recipe-1-thumb-1500xauto-431710.jpg',
+          [
+              new Ingredient('Beans', 10),
+              new Ingredient('Carrot', 5),
+              new Ingredient('Ricotta cheese', 2)
+          ]),
+      new Recipe('30-Minute Pressure Cooker Chicken With Chickpeas, Tomatoes, and Chorizo',
+          'The secret to this recipe is the addition of canned fire-roasted tomatoes, which add lots of flavor with hardly any effort. ' +
           'We bolster the dish`s savory side with Spanish chorizo and smoked paprika and add in chicken and canned' +
-          ' chickpeas for a dinner that`s as hearty as it is easy.', 'http://www.seriouseats.com/images/2015/09/' +
-          '20150914-pressure-cooker-recipes-roundup-09.jpg')
+          ' chickpeas for a dinner that`s as hearty as it is easy.',
+          'http://www.seriouseats.com/images/2015/09/20150914-pressure-cooker-recipes-roundup-09.jpg',
+          [
+              new Ingredient('Chicken', 1),
+              new Ingredient('Chickpeas', 1),
+              new Ingredient('Tomatoes', 6),
+              new Ingredient('Chorizo', 1)
+          ])
     ];
 
     getRecipes() {
