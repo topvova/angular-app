db5bd414de59b043fd096ddab28e47cd183b08cc
topvova <topvova@gmail.com>
2017-07-09 10:06:00 +0300

    Move recipes array to the Recipeservise (l99)

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,5 +1,6 @@
 import {Component, EventEmitter, OnInit, Output} from '@angular/core';
 import { Recipe } from '../recipe.model';
+import { RecipeService } from '../recipe.service';
 
 @Component({
   selector: 'app-recipe-list',
@@ -8,24 +9,14 @@ import { Recipe } from '../recipe.model';
 })
 
 export class RecipeListComponent implements OnInit {
-  recipes: Recipe[] = [
-      new Recipe('Easy Fava Bean and Carrot Salad With Ricotta Recipe',
-          'This quick and easy salad takes some of spring`s best produce—bright green fava beans and tender young' +
-          ' carrots—and does just the right thing to them, which is very little. With a simple vinaigrette, some' +
-          ' thinly sliced shallots, and a generous serving of fresh ricotta cheese to add richness, it`s all they' +
-          ' need to taste their best.', 'http://www.seriouseats.com/recipes/assets_c/2016/05/20160503-fava-carrot-' +
-          'ricotta-salad-recipe-1-thumb-1500xauto-431710.jpg'),
-      new Recipe('30-Minute Pressure Cooker Chicken With Chickpeas, Tomatoes, and Chorizo', 'The secret to this recipe' +
-          ' is the addition of canned fire-roasted tomatoes, which add lots of flavor with hardly any effort. ' +
-          'We bolster the dish`s savory side with Spanish chorizo and smoked paprika and add in chicken and canned' +
-          ' chickpeas for a dinner that`s as hearty as it is easy.', 'http://www.seriouseats.com/images/2015/09/' +
-          '20150914-pressure-cooker-recipes-roundup-09.jpg')
-  ];
+  recipes: Recipe[];
   @Output() recipeWasSelected = new EventEmitter<Recipe>();
 
-  constructor() { }
+  constructor(private recipeService: RecipeService) { }
 
   ngOnInit() {
+      // get copy of recipes
+      this.recipes = this.recipeService.getRecipes();
   }
 
   onRecipeSelected(recipe: Recipe) {
