d064e85e92bb40fba09a4b973ee15adbecabfdf8
topvova <topvova@gmail.com>
2017-07-12 09:40:13 +0300

    Add getRecipe method to RecipeService, assign arguments for recipe when RecipeDetail component initializes, remove unnecessary methods and imports in RecipeItem (l141)

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -38,6 +38,10 @@ export class RecipeService {
         return this.recipes.slice();
     }
 
+    getRecipe(id: number) {
+        return this.recipes[id];
+    }
+
     addIngredientsToShoppingList(ingredients: Ingredient[]) {
         this.shopListService.addIngredients(ingredients);
     }
