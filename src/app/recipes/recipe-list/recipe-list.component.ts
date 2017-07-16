59b7a5902c3d96d52255a8b2fe1eecaf67ad8dc4
topvova <topvova@gmail.com>
2017-07-16 15:50:10 +0300

    Add onDeleteIngredient method, fix some bugs(l210)

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,7 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 import { Recipe } from '../recipe.model';
 import { RecipeService } from '../recipe.service';
 import { ActivatedRoute, Router } from '@angular/router';
+import { Subscription } from 'rxjs/Subscription';
 
 @Component({
   selector: 'app-recipe-list',
@@ -9,14 +10,15 @@ import { ActivatedRoute, Router } from '@angular/router';
   styleUrls: ['./recipe-list.component.css']
 })
 
-export class RecipeListComponent implements OnInit {
+export class RecipeListComponent implements OnInit, OnDestroy {
   recipes: Recipe[];
+  subscription: Subscription;
 
   constructor(private recipeService: RecipeService, private router: Router, private route: ActivatedRoute) { }
 
   ngOnInit() {
       // get copy of recipes
-      this.recipeService.recipesChanged
+      this.subscription = this.recipeService.recipesChanged
           .subscribe(
               (recipes: Recipe[]) => {
                   this.recipes = recipes;
@@ -27,5 +29,9 @@ export class RecipeListComponent implements OnInit {
 
   onNewRecipe() {
       this. router.navigate(['new'], {relativeTo: this.route}); // recipes/new
+  }
+
+  ngOnDestroy() {
+      this.subscription.unsubscribe();
   }
 }
