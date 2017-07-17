f147b567f55a00d7a7299967dc39f818dfb1fc9d
topvova <topvova@gmail.com>
2017-07-17 11:15:55 +0300

    Transform the response from server to prevent errors witn ingredients (l235)

--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -2,6 +2,7 @@ import {Injectable} from '@angular/core';
 import {Http, Response} from '@angular/http';
 import {RecipeService} from '../recipes/recipe.service';
 import {Recipe} from '../recipes/recipe.model';
+import 'rxjs/add/operator/map';
 
 @Injectable()
 export class DataStorageService {
@@ -12,9 +13,20 @@ export class DataStorageService {
     }
     getRecipes() {
         this.http.get('https://recipe-book-ef00e.firebaseio.com/recipes.json')
-            .subscribe(
+            .map(
                 (response: Response) => {
                     const recipes: Recipe[] = response.json();
+                    for (let recipe of recipes) {
+                        if (!recipe['ingredients']) {
+                            console.log(recipe);
+                            recipe['ingredients'] = [];
+                        }
+                    }
+                    return recipes;
+                }
+            )
+            .subscribe(
+                (recipes: Recipe[]) => {
                     this.recipeService.setRecipes(recipes);
                 }
             );
