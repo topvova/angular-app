dcd9013db83ac0d648f6c13346672a6d5e009352
topvova <topvova@gmail.com>
2017-07-17 19:30:15 +0300

    Install firebase, initialize the App on AppComponent, create method to signup a user (239)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,21 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import * as firebase from 'firebase';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   loadedChapter = 'recipe';
 
   onNavigate(chapter: string) {
     this.loadedChapter = chapter;
+  }
+  ngOnInit() {
+    firebase.initializeApp({
+      apiKey: 'AIzaSyBmNQ7QmG84jpiECh4ODWJPC06-iYPpImQ',
+      authDomain: 'recipe-book-ef00e.firebaseapp.com',
+    })
   }
 }
